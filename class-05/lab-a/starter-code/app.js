99e1adcfe6cd9b4293ed07053720729803167d5c
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-31 14:43:34 +0200

    solve sum-multi

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -61,11 +61,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+var sum = a+b+c;
+var multi = a*b*c;
+var array = [
+    sum,multi,"4 and 7 and 5 sum to 16.","The product of 4 and 7 and 5 is 140."
+];
+return array;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
